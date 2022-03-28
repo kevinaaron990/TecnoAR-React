fb3ba8edc71dba70bf3b977cb7167c30511f5542
kevinaaron990 <kevinhorvath39@gmail.com>
2022-03-28 18:03:26 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { hover } from '@testing-library/user-event/dist/hover';
+import React from 'react';
+import NabVar from './componentes/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <NabVar/>
+    
   );
 }
+
 
 export default App;
---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import './NavVar.css'
+import * as ReactBootstrap from "react-bootstrap"
+
+export default function NabVar(){
+    return(
+       <nav>
+         
+           <ReactBootstrap.Navbar collapseOnSelect expand="xl" bg="dark" variant="dark">
+                <ReactBootstrap.Container>
+                <ReactBootstrap.Navbar.Brand href="#home" className="titulo">
+        
+                    <h1>Tecno<span>AR</span></h1>
+                 </ReactBootstrap.Navbar.Brand>
+      
+                <ReactBootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <ReactBootstrap.Navbar.Collapse id="responsive-navbar-nav">
+                    <ReactBootstrap.Nav className="me-auto">
+                    </ReactBootstrap.Nav>
+                    <ReactBootstrap.Nav className="menu">
+                        <ReactBootstrap.Nav.Link href="#" className="menu__item">Inicio</ReactBootstrap.Nav.Link>
+                        <ReactBootstrap.Nav.Link href="#" className="menu__item">Nosotros</ReactBootstrap.Nav.Link>
+                        <ReactBootstrap.Nav.Link href="#" className="menu__item">Contacto</ReactBootstrap.Nav.Link>
+                    </ReactBootstrap.Nav>
+                </ReactBootstrap.Navbar.Collapse>
+                </ReactBootstrap.Container>
+            </ReactBootstrap.Navbar>
+       </nav>
+    
+    );}
